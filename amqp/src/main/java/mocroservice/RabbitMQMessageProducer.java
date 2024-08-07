6120a8acdd458d761c510b5caab697ceda6a37bc
hasanabbasov <43010697+hasanabbasov@users.noreply.github.com>
2024-08-08 00:18:40 +0300

    implemented rubbitMq

---
+++ amqp/src/main/java/mocroservice/RabbitMQMessageProducer.java
@@ -0,0 +1,23 @@
+package mocroservice;
+
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class RabbitMQMessageProducer {
+
+    private final AmqpTemplate amqpTemplate;
+
+    public void publish(Object payload, String exchange, String routingKey) {
+        log.info("Publish {} , {}, {}", exchange,routingKey, payload);
+        amqpTemplate.convertAndSend(exchange,routingKey, payload);
+        log.info("Publish {} , {}, {}", exchange,routingKey, payload);
+
+    }
+
+}
